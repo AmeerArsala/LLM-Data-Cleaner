fe05c35c9203406c60098097e194c808021c6e5d
AmeerArsala <ameer.arsala03@gmail.com>
2023-11-13 13:11:55 -0800

    fixed bug, added interface

---
+++ src/prompts/prompt_tuning.py
@@ -0,0 +1,46 @@
+from langchain.llms.base import LLM
+from langchain.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
+import ChatIO
+
+
+def as_template(name: str):
+    return "".join(["{", name, "}"])
+
+"""
+To chain with LCEL: let's say you have a PromptTuner object called prompt_tuner
+the chain would be like so:
+
+chain = prompt_tuner.chain | prompt_tuner2.chain | ...
+
+and you get the idea
+"""
+class PromptTuner:
+    def __init__(self, llm: LLM, sys_initial_prompt: str, few_shot_examples: list[ChatIO], input_name="input", output_name="output"):
+        super()
+        self.llm = llm
+        self._sys_initial_prompt = sys_initial_prompt
+        self._few_shot_examples = few_shot_examples
+        self.input_name = input_name
+        self.output_name = output_name
+
+        # Few Shot Learning
+        example_few_shot_prompt = ChatPromptTemplate.from_messages([
+            ("human", as_template(input_name)),
+            ("ai", as_template(output_name))
+        ])
+
+        few_shot_examples_dict = ChatIO.make_chat(few_shot_examples, input_name, output_name)
+        few_shot_prompt = FewShotChatMessagePromptTemplate(example_prompt=example_few_shot_prompt, examples=few_shot_examples_dict)
+
+        # Final Chat Prompt: incorporate the few-shot learning
+        self.chat_prompt_template = ChatPromptTemplate.from_messages([
+            ("system", sys_initial_prompt),
+            few_shot_prompt,
+            ("human", as_template(input_name))
+        ])
+
+        self.chain = (self.chat_prompt_template | self.llm)
+    
+    def chat(self, input: str):
+        return self.chain.invoke({self.input_name: input})
+
